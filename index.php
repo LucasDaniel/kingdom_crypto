3017ec57a5c8f9217dbffb0594a34363526d5e10
LucasDaniel <lucasdanielbeltrame@hotmail.com>
2022-01-23 18:36:39 -0300

    Versão alpha do site

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-echo "Inicio";
+require_once("config/imports.php");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title><?php echo $GLOBAL['title'] ?></title>
+    <meta charset="utf-8">
+  </head>
+  <?php require_once("view/cadastro_beta.php"); ?>
+  <?php require_once("config/importsjs.html"); ?>
+</html>
