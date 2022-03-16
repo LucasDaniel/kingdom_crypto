feb3ad276aaff279a3d15f1d21089e1577234a4a
LucasDaniel <lucasdanielbeltrame@hotmail.com>
2022-03-16 18:41:08 -0300

    Adicionando rede social do telegram

--- view/redes_sociais.php
+++ view/redes_sociais.php
@@ -1,17 +1,22 @@
 <div class="row m-top-1em">
   <div class="bgc-transparent card d-inline p-1em">
     <div class="f-left m-right-1em">
-      <a href="https://twitter.com/kingrespectcryp">
+      <a href="https://twitter.com/kingrespectcryp" target="_blank">
         <img src="https://kingrespectcrypto.com/resources/images/twitter_icon.png" width="32px">
       </a>
     </div>
     <div class="f-left m-right-1em">
-      <a href="https://www.youtube.com/channel/UCyhZdrEARIQjk-QnBaLTf2w/videos">    
+      <a href="https://www.youtube.com/channel/UCyhZdrEARIQjk-QnBaLTf2w/videos" target="_blank">    
         <img src="https://kingrespectcrypto.com/resources/images/youtube_icon.png" width="32px">
       </a>
     </div>
+    <div class="f-left m-right-1em">
+      <a href="https://t.me/kingrespectcrypto" target="_blank">    
+        <img src="https://kingrespectcrypto.com/resources/images/telegram_icon.png" width="32px">
+      </a>
+    </div>
     <div class="f-left">
-      <a href="https://lucas-daniel-beltrame.gitbook.io/king-respect-crypto/">    
+      <a href="https://lucas-daniel-beltrame.gitbook.io/king-respect-crypto/" target="_blank">    
         <img src="https://kingrespectcrypto.com/resources/images/whitepaper_icon.png" width="32px">
       </a>
     </div>
