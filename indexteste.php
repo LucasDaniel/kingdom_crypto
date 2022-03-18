341df2f60dbf0a475cdca30105e5607f3435d0aa
LucasDaniel <lucasdanielbeltrame@hotmail.com>
2022-03-18 17:09:38 -0300

    modificando cadastro beta

--- indexteste.php
+++ indexteste.php
@@ -131,7 +131,6 @@ $DICIONARIO['play'] = "Play";
                 </div>
             </div>
         </div>
-
         <div class="row m-top-1em">
             <div class="col-12">
                 <div class="login-logo t_white">
@@ -142,35 +141,6 @@ $DICIONARIO['play'] = "Play";
                     Your browser does not support HTML video.
                 </video>
             </div>
-        </div>
-        <div class="row m-top-1em">
-            <div class="bgc-transparent card d-inline p-1em">
-                <div class="f-left m-right-1em">
-                    <a href="https://twitter.com/kingrespectcryp" target="_blank">
-                        <img src="https://kingrespectcrypto.com/resources/images/twitter_icon.png" width="32px">
-                    </a>
-                </div>
-                <div class="f-left m-right-1em">
-                    <a href="https://www.youtube.com/channel/UCyhZdrEARIQjk-QnBaLTf2w/videos" target="_blank">    
-                        <img src="https://kingrespectcrypto.com/resources/images/youtube_icon.png" width="32px">
-                    </a>
-                </div>
-                <div class="f-left m-right-1em">
-                    <a href="https://t.me/kingrespectcrypto" target="_blank">    
-                        <img src="https://kingrespectcrypto.com/resources/images/telegram_icon.png" width="32px">
-                    </a>
-                </div>
-                <div class="f-left">
-                    <a href="https://lucas-daniel-beltrame.gitbook.io/king-respect-crypto/" target="_blank">    
-                        <img src="https://kingrespectcrypto.com/resources/images/whitepaper_icon.png" width="32px">
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="row m-top-1em t_white">
-            <a href="https://kingrespectcrypto.com/privacypolitics.php" target="_blank">
-                Privacy Politics
-            </a>
         </div>
     </body>
 
--- view/cadastro_beta.php
+++ view/cadastro_beta.php
@@ -1,48 +1,103 @@
 <?php  ?>
 <body class="hold-transition login-page background_index">
-  <div class="row">
-    <div class="col-8">
-      <div class="login-logo t_white">
-        <?php echo $DICIONARIO['presetation_video'] ?>
+  <div class="menu_principal">
+      <div class="bt_menu m-left-0">
+          Play Game
       </div>
-      <video width="700" controls autoplay>
-        <source src="https://kingrespectcrypto.com/resources/video/apresentacao.mp4" type="video/mp4">
-        Your browser does not support HTML video.
-      </video>
-    </div>
-    <div class="col-4">
-      <div class="login-box">
-        <div class="login-logo t_white">
-          <?php echo $GLOBAL['title'] ?>
-        </div>
-        <div class="card">
-          <div class="card-body login-card-body">
-          <p class="login-box-msg"><?php echo $DICIONARIO['discovery_project'] ?></p>
-            <p class="login-box-msg"><?php echo $DICIONARIO['beta_register'] ?></p>
-            <form action="https://kingrespectcrypto.com/controller/envio_email.php" method="post">
-              <div class="input-group mb-3">
-                <input type="email" name="email" class="form-control" placeholder="<?php echo $DICIONARIO['email'] ?>">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-envelope"></span>
+      <div class="bt_menu">
+          <a href="https://lucas-daniel-beltrame.gitbook.io/king-respect-crypto/" target="_blank">
+              Whitepaper
+          </a>
+      </div>
+      <div class="bt_menu">
+          Pre sale
+      </div>
+      <div class="bt_menu">
+          <a href="https://lucasgamestudio.com.br/" target="_blank">
+              Created By (Dev)
+          </a>
+      </div>
+      <div class="bt_menu">
+          Contact
+      </div>
+  </div>
+
+  <!-- Inicio slideshow -->
+  <!-- Slideshow container -->
+  <div class="slideshow-container">
+
+      <!-- Full-width images with number and caption text -->
+      <div class="mySlides fade">
+          <img src="resources/images/back1.png" style="width:1043px">
+          <div class="text">1 / 3</div>
+      </div>
+
+      <div class="mySlides fade">
+          <img src="resources/images/back2.png" style="width:1043px">
+          <div class="text">2 / 3</div>
+      </div>
+
+      <div class="mySlides fade">
+          <img src="resources/images/back3.png" style="width:1043px">
+          <div class="text">3 / 3</div>
+      </div>
+  </div>
+  <br>
+
+  <!-- The dots/circles -->
+  <div style="text-align:center; display:none;">
+      <span class="dot" onclick="currentSlide(1)"></span>
+      <span class="dot" onclick="currentSlide(2)"></span>
+      <span class="dot" onclick="currentSlide(3)"></span>
+  </div>
+
+  <!-- Fim slideshow -->
+
+  <div class="beta_test row">
+      <div class="col-12">
+          <div class="login-box">
+              <div class="login-logo t_white">
+                  Sign up for the beta test
+              </div>
+              <div class="card">
+                  <div class="card-body login-card-body">
+                  <p class="login-box-msg"><?php echo $DICIONARIO['discovery_project'] ?></p>
+                      <p class="login-box-msg"><?php echo $DICIONARIO['beta_register'] ?></p>
+                      <form action="https://kingrespectcrypto.com/controller/envio_email.php" method="post">
+                          <div class="input-group mb-3">
+                              <input type="email" name="email" class="form-control" placeholder="<?php echo $DICIONARIO['email'] ?>">
+                              <div class="input-group-append">
+                                  <div class="input-group-text">
+                                      <span class="fas fa-envelope"></span>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="row m-left-0px">
+                              <div class="g-recaptcha" name="recaptcha" data-sitekey="<?php echo $GLOBAL['site_recaptcha']; ?>"></div>
+                          </div>
+                          <div class="row m-top-12px">
+                              <div class="col-8">
+                              </div>
+                              <div class="col-4">
+                                  <button type="submit" class="btn btn-primary btn-block" name="submit" onclick="return valida()"><?php echo $DICIONARIO['sign_in'] ?></button>
+                              </div>
+                          </div>
+                      </form>
                   </div>
-                </div>
               </div>
-              <div class="row m-left-0px">
-                <div class="g-recaptcha" name="recaptcha" data-sitekey="<?php echo $GLOBAL['site_recaptcha']; ?>"></div>
-              </div>
-              <div class="row m-top-12px">
-                <div class="col-8">
-                </div>
-                <div class="col-4">
-                  <button type="submit" class="btn btn-primary btn-block" name="submit" onclick="return valida()"><?php echo $DICIONARIO['sign_in'] ?></button>
-                </div>
-              </div>
-            </form>
           </div>
-        </div>
       </div>
-    </div>
+  </div>
+  <div class="row m-top-1em">
+      <div class="col-12">
+          <div class="login-logo t_white">
+              <?php echo $DICIONARIO['presetation_video'] ?>
+          </div>
+          <video width="700" controls autoplay>
+              <source src="https://kingrespectcrypto.com/resources/video/apresentacao.mp4" type="video/mp4">
+              Your browser does not support HTML video.
+          </video>
+      </div>
   </div>
   <?php require_once("redes_sociais.php"); ?>
 </body>
@@ -53,5 +108,36 @@
       alert("Recaptcha not checked");
       return false;
     } else return true;
+  }
+
+  var slideIndex = 0;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+      showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+      setTimeout(showSlides, 5000); // Change image every 2 seconds
   }
 </script>
